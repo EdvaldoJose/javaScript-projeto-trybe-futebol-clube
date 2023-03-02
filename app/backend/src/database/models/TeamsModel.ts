9b389c8e1bd85a2c04449a7220603b7b5e001eee
Edvaldo Jose <edvaldojose003@gmail.com>
2023-03-02 09:50:57 -0300

    Requisito 01 concluido

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,26 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Teams.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING(100),
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'Teams',
+  timestamps: false,
+});
+
+export default Teams;
