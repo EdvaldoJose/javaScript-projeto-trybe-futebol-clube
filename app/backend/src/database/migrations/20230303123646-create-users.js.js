992c04ecb634abb6c17f54460cb1f64188ca25db
Edvaldo Jose <edvaldojose003@gmail.com>
2023-03-03 10:30:43 -0300

    Requisito 06 concluido

---
+++ app/backend/src/database/migrations/20230303123646-create-users.js.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users',  {
+      id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
---
+++ app/backend/src/interface/IUserCredential.ts
@@ -0,0 +1,4 @@
+export default interface IUserCredential {
+  email: string;
+  password: string;
+}
