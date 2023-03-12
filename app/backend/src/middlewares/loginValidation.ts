823d11fc99ad4679387306db70ccb9b0f02e7102
Edvaldo Jose <edvaldojose003@gmail.com>
2023-03-11 21:33:05 -0300

    Correcao dos tests 03

--- app/backend/src/middlewares/loginValidation.ts
+++ app/backend/src/middlewares/loginValidation.ts
@@ -2,22 +2,31 @@ import { NextFunction, Request, Response } from 'express';
 
 import Joi = require('joi');
 
-const MESSAGE_ERROR = 'All fields must be filled';
+import CustomError from '../utils/statusError';
 
+// const MESSAGE_ERROR = 'Invalid email or password';
+
+// const loginShema = Joi.object({
+//   email: Joi.string().email().required(),
+//   password: Joi.string().min(6).required(),
+
+// }).error();
 const loginShema = Joi.object({
-  email: Joi.string().email().required().messages({
-    'string.empty': MESSAGE_ERROR,
-    'any.required': MESSAGE_ERROR,
-  }),
-  password: Joi.string().min(6).required().messages({
-    'string.empty': MESSAGE_ERROR,
-    'any.required': MESSAGE_ERROR,
-  }),
+  email: Joi.string().email().required().error(new Error('Invalid email or password')),
+  password: Joi.string().min(6).required().error(new Error('Invalid email or password')),
 });
 
 const validateLogin = async (req: Request, res: Response, next: NextFunction) => {
   const { error } = loginShema.validate(req.body);
-  const { email } = req.body;
+  const { email, password } = req.body;
+
+  if (!email) {
+    throw new CustomError('All fields must be filled', 400);
+  }
+
+  if (!password) {
+    throw new CustomError('All fields must be filled', 400);
+  }
 
   if (email.length === 0) {
     return res.status(400).json({ message: error?.message });
